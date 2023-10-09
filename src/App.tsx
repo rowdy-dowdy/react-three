52e8950f67aceefa38506a0c0428781f5940c41c
Viet Hung <viet.hung.2898@gmail.com>
2023-10-09 14:31:10 +0700

    add

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Canvas } from '@react-three/fiber'
+
+function App() {
+  return (
+    <div className="w-full h-screen">
+      <Canvas>
+      </Canvas>
+    </div>
+  )
+}
+
+export default App
